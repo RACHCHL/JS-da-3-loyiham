11ad8e823f9c1d33c290e222303d45026eecba11
Rasulbek <quvondiqovrasulbek054@gmail.com>
2024-12-01 02:15:47 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+var n= prompt("Ismingizni kiriting")
+var b="Rasulbek"
+
+if(n == b){
+    alert("Hello BOSS!")
+}
+else if(n!=b && isNaN(n)){
+    alert("Hello guest")
+}
+else if(!isNaN(n)){
+    alert("Isim faqat harflardan iborat bolishi kerak")
+}
+
+
+
+
+
+
+
+
+
