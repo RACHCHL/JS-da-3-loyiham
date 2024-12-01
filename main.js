4398fab92792fbbd5dc716200f7f8ec619558aec
Rasulbek <quvondiqovrasulbek054@gmail.com>
2024-12-01 23:38:53 +0500

    oila azolari bilan

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-var n= prompt("Ismingizni kiriting")
+var n= prompt("Ismingizni kiriting. masalan 'Abubakr' ko'rinishida")
 var b="Rasulbek"
-
+var h="Ruslan"
+var j="Jaxongul"
+var z="Zilola"
+var k="Nozima"
+var l="Nozimaxon"
 if(n == b){
     alert("Hello BOSS!")
+}
+else if(n==h){
+    alert("Hello Rasulbek's father")
+}
+else if(n==j){
+    alert("Hello Rasulbek's mather")
+}
+else if(n==z){
+    alert("salom Rasulbekning suyukli singilchasi")
+    
+}
+else if(n==k || n==l){
+      alert("Salom Rasulbekning suyukli opajonisi")
 }
 else if(n!=b && isNaN(n)){
     alert("Hello guest")
@@ -10,6 +27,7 @@ else if(n!=b && isNaN(n)){
 else if(!isNaN(n)){
     alert("Isim faqat harflardan iborat bolishi kerak")
 }
+
 
 
 
